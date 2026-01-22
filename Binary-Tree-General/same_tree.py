1e9f138b0a24b4541da51dc68d2292fdae110d57
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-01-21 18:28:29 -0800

    same tree question

--- Binary-Tree-General/same_tree.py
+++ Binary-Tree-General/same_tree.py
@@ -3,6 +3,26 @@ Problem: Same Tree
 LeetCode: https://leetcode.com/problems/same-tree/
 Difficulty: Easy
 Approach:
-Time:
-Space:
+Time: O(n) - each node is visited once
+Space: O(n) - worst case. O(h) - best case. I say O(h) and not O(logn) because space complexity depends on how deep the recursion goes and that will always depend on the max depth of the tree which is h. O(logn) would be the answer if we believed the tree was perfectly balanced as in that case h = logn 
 """
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        # both empty
+        if p is None and q is None:
+            return True
+        # if different structure
+        if (p is None and q is not None) or (p is not None and q is None):
+            return False
+        # if same structure check the value
+        if p.val != q.val:
+            return False
+        else:
+            return (self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right))
