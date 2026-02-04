752b6360016dd674272750ebefcdad0d6604ffa7
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-02-03 17:36:49 -0800

    sol

--- Array-String/best_time_to_buy_and_sell_stock.py
+++ Array-String/best_time_to_buy_and_sell_stock.py
@@ -4,4 +4,16 @@ LeetCode: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 Difficulty: Easy
 Approach:
 Time:
-Space
+Space:
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        i = 0
+        while i < len(prices) - 1:
+            j = i + 1
+            for k in range(j, len(prices)):
+                if (prices[k] - prices[i]) > profit:
+                    profit = prices[k] - prices[i]
+            i += 1
+        return profit
