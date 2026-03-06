7fcedce6963289920fc933bd83596a5fd483495a
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-05 17:52:53 -0800

    done

--- Binary-Tree-General/symmetric_tree.py
+++ Binary-Tree-General/symmetric_tree.py
@@ -7,3 +7,17 @@ Time: O(n)
 Space: O(h)
 """
 
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True
+        else:
+            return self.mirror(root.left, root.right)
+        
+    def mirror(self, left, right) -> bool:
+        if not left and not right:
+            return True
+        elif not left or not right:
+            return False
+        else:
+            return (left.val == right.val) and self.mirror(left.left, right.right) and self.mirror(left.right, right.left)
