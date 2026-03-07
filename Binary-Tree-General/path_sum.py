4b21f1ec550b1f284ec13b6de7164f7bc5dce32a
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-07 13:46:29 -0800

    solved

--- Binary-Tree-General/path_sum.py
+++ Binary-Tree-General/path_sum.py
@@ -6,3 +6,15 @@ Approach:
 Time:
 Space:
 """
+
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        # no chance
+        if not root:
+            return False
+        # is a leaf
+        targetSum -= root.val
+        if not root.left and not root.right and targetSum == 0:
+            return True
+        else:
+            return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
