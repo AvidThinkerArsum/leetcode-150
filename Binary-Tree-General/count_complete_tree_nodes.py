dbf379a675aff2f50f0d48ad456f9879333648d7
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-07 14:11:52 -0800

    ok

--- Binary-Tree-General/count_complete_tree_nodes.py
+++ Binary-Tree-General/count_complete_tree_nodes.py
@@ -2,7 +2,14 @@
 Problem: Count Complete Tree Nodes
 LeetCode: https://leetcode.com/problems/count-complete-tree-nodes/
 Difficulty: Easy
-Approach:
+Approach: pretty straightforward
 Time:
 Space:
 """
+
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
