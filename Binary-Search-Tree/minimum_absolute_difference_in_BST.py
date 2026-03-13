cb6e3b07f7cb5f7dd43b48dd5c750d453e029b02
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-13 05:54:18 -0700

    done

--- Binary-Search-Tree/minimum_absolute_difference_in_BST.py
+++ Binary-Search-Tree/minimum_absolute_difference_in_BST.py
@@ -2,7 +2,24 @@
 Problem: Minimum Absolute Difference in BST
 LeetCode: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 Difficulty: Easy
-Approach: 
+Approach: A BST arranged in order can be easily used to find such a difference between successive values. the inorder function does not need to return anything and is only there to update our variable values. 
 Time: 
 Space:
 """
+
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        self.prev = None
+        self.min_diff = float('inf')
+        self.inOrder(root)
+        return self.min_diff
+
+    def inOrder(self, root):    
+        if not root:
+            return
+        else:
+            self.inOrder(root.left)
+            if self.prev is not None:
+                self.min_diff = min(self.min_diff, root.val - self.prev)
+            self.prev = root.val
+            self.inOrder(root.right)
