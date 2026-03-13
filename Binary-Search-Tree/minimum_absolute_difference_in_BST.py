fcc77a2c11bdb97b3b92fd655e8319287f141f24
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-13 06:00:00 -0700

    time and space complexities done

--- Binary-Search-Tree/minimum_absolute_difference_in_BST.py
+++ Binary-Search-Tree/minimum_absolute_difference_in_BST.py
@@ -3,8 +3,8 @@ Problem: Minimum Absolute Difference in BST
 LeetCode: https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 Difficulty: Easy
 Approach: A BST arranged in order can be easily used to find such a difference between successive values. the inorder function does not need to return anything and is only there to update our variable values. 
-Time: 
-Space:
+Time: O(n) - as each node is visited once 
+Space: O(h) - this depends on how much extra memory exists at the same time. and extra memory is a recursive call. so how many max recursive calls can exist at the same time and that is the depth of the tree because you keep going left. it is true that when you hit the end you will go to the right as well but then the left calls are taken off the stack and so the average max number of recursive calls you are going to have are equal to the depth. in the worst case though you might have O(n) if all the node are on one side.
 """
 
 class Solution:
