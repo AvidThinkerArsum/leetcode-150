8fe6c63ca2927282bd9877962e8bb45e9c94ddcb
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-17 18:02:27 -0700

    solution

--- Array-String/longest_common_prefix.py
+++ Array-String/longest_common_prefix.py
@@ -6,3 +6,12 @@ Approach:
 Time: 
 Space: 
 """
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range( len ( strs[0] ) ):
+            char = strs[0][i]
+            for str in strs:
+                if (i > len(str) - 1) or str[i] != char:
+                    return strs[0][:i]
+        return strs[0]
