d17448442121da6b1e8a7b6d0d5cc01fbfbcde9e
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-17 18:39:55 -0700

    starting

---
+++ Array-String/find_the_index_of_the_first_occurrence_in_a_string.py
@@ -0,0 +1,8 @@
+"""
+Problem: Find the Index of the First Occurrence in a String
+LeetCode: https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/
+Difficulty: Easy
+Approach:
+Time:
+Space:
+"""
