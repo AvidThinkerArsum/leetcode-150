999cde10c5509cb3eea24c62058a24ad024a8281
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-03-20 16:55:40 -0700

    done

---
+++ Hashmap/group_anagrams.py
@@ -0,0 +1,8 @@
+"""
+Problem: Group Anagrams
+LeetCode: https://leetcode.com/problems/group-anagrams/
+Difficulty: Easy
+Approach: Use a hashmap. always think about how can I unite these values so to have a common key and in this case we can order them to group them.
+Time:
+Space:
+"""
