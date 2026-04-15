494037a9b5ec744a3d68bc42f9529caba217e2a5
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-04-15 15:09:20 -0700

    done

--- Binary-Tree-General/sum_root_to_leaf_numbers.py
+++ Binary-Tree-General/sum_root_to_leaf_numbers.py
@@ -6,3 +6,22 @@ Time:
 Space:
 """
 
+class Solution:
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        if not root.left and not root.right:
+            return root.val
+
+        return self.pathFinder(root.left, root.val) + self.pathFinder(root.right, root.val)
+
+    def pathFinder(self, node, current_number: int) -> int:
+        if not node:
+            return 0
+        else:
+            current_number = (current_number * 10) + node.val
+        if not node.left and not node.right:
+            return current_number
+        else:
+            return self.pathFinder(node.left, current_number) + self.pathFinder(node.right, current_number)
