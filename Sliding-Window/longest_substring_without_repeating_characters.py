67926c381daf034fdf3ca1271e90815884bb8ded
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-04-19 21:14:37 -0700

    added explanation

---
+++ Sliding-Window/longest_substring_without_repeating_characters.py
@@ -0,0 +1,8 @@
+"""
+Problem: Longest Substring Without Repeating Characters
+LeetCode: https://leetcode.com/problems/longest-substring-without-repeating-characters/
+Difficulty: Medium
+Approach: Use two pointer and hashmaps
+Time: O(N)
+Space: Don't know
+"""
