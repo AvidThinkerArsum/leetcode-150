758c1d4d1c7ada7f4a3425e2f01f326ee09ef8a8
Arsum Nadeem Chaudhary <arsumnadeemch16203@gmail.com>
2026-04-24 19:18:01 -0700

    added time and space complexity

--- Matrix/valid_sudoku.py
+++ Matrix/valid_sudoku.py
@@ -2,9 +2,9 @@
 Problem: Valid Sudoku
 LeetCode: https://leetcode.com/problems/valid-sudoku/
 Difficulty: Medium
-Approach:
-Time:
-Space:
+Approach: Handle each condition. Matrix question will rely on use intelligent and precise looping.
+Time: O(N^2) - The row check finished first and then the column check starts. They don't run at the same time which is why we add them. Now, for one of them we have 9x9 so N^2
+Space: O(N). Again at a single moment only one of our three main checks is running as they don't run in paralell. Now, it is the same set being used over and over. Take the example of the row check. The max size of the set is 9 and for each row we can make a new set and so space complexity which is the max space used at any one point is going to be O(N)
 """
 
 class Solution:
